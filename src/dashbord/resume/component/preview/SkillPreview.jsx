87397ab19279305bb6deb1bfcb9049a8229cfca9
Shubham Thakur <shubham.20102005.bca22@engg.hgpi.in>
2024-09-21 17:41:26 +0530

    Update SkillPreview.jsx

--- src/dashbord/resume/component/preview/SkillPreview.jsx
+++ src/dashbord/resume/component/preview/SkillPreview.jsx
@@ -7,15 +7,15 @@ const SkillPreview = ({ resumeInfo }) => {
             <hr style={{ borderColor: resumeInfo?.themeColor }} className="border mt-2 mb-2" />
 
             {/* Grid layout for skills */}
-            <div id="skill" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 w-[90%] mx-auto">
+            <div id="skill" className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-6 w-[90%] mx-auto">
                 {
                     resumeInfo?.skills?.map((skill, i) => (
-                        <div id="data" className="flex flex-col items-start" key={i}>
+                        <div id="data" className="flex flex-col flex-wrap items-start" key={i}>
                             {/* Skill Name */}
                             <h2 className="font-medium">{skill?.name}</h2>
                             
                             {/* Rating Bar */}
-                            <div className="w-full bg-gray-300 h-[4px] rounded-full mt-3">
+                            <div className="w-full   bg-gray-300 h-[4px] rounded-full mt-3">
                                 <div
                                     className="h-[4px] rounded-full"
                                     style={{
